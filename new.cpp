f7dc82b8ad7db27b9cb9424eb10f252624094ece
Laksshay <laksshaysehrawat28@gmail.com>
2022-05-20 12:56:28 +0530

    ~

--- new.cpp
+++ new.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 int main()
 {
+    // doubt 1-
+    // int arr[] = {1, 2, 3, 4};
+    // cout << &arr[0] << endl;
+    // cout << &arr[1] << endl;
+    // cout << "size of int is " << ((&arr[1]) - (&arr[0])) << endl;
+    // why is the answer not 4?
+    string str;
+    cout << "enter a string : ";
+    getline(cin, str);
+    cout << str << endl;
 }

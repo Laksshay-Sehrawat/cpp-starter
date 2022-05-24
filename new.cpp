e5d5b0871722cc57f2db24b688ef2ddb9914102c
Laksshay <laksshaysehrawat28@gmail.com>
2022-05-24 10:38:02 +0530

    ~

--- new.cpp
+++ new.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+struct person
+{
+    int age;
+    char name[10];
+    int salary;
+};
 
 int main()
 {
@@ -9,8 +15,27 @@ int main()
     // cout << &arr[1] << endl;
     // cout << "size of int is " << ((&arr[1]) - (&arr[0])) << endl;
     // why is the answer not 4?
-    string str;
-    cout << "enter a string : ";
-    getline(cin, str);
-    cout << str << endl;
+
+    // person Laksshay;
+    // cout << "enter age - ";
+    // cin >> Laksshay.age;
+    // cout << "enter name - " << endl;
+    // cin.get(Laksshay.name, 30);
+    // cout << "age is " << Laksshay.age << " name is " << Laksshay.name;
+
+    // person p1;
+
+    // cout << "Enter Full name: ";
+    // cin.get(p1.name, 50);
+    // cout << "Enter age: ";
+    // cin >> p1.age;
+
+    // cout << "\nDisplaying Information." << endl;
+    // cout << "Name: " << p1.name << endl;
+    // cout << "Age: " << p1.age << endl;
+    int n1, n2;
+    cout << "enter number 1 ";
+    cin >> n1;
+    cout << "enter numbr 2 ";
+    cin >> n2;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    // Laksshay.name = "laksshay";

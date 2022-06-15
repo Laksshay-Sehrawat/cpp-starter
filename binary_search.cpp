e92874f6230ce36a6cbafb40984960843b2cb117
Laksshay <laksshaysehrawat28@gmail.com>
2022-06-15 23:35:07 +0530

    binary

---
+++ binary_search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int binary_search(int arr[], int size, int key)
+{
+    int start = 0, end = size - 1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (arr[mid] < key)
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 10, 23, 56, 76, 34};
+    cout << "the index of number is - " << binary_search(arr, 9, 101) << endl;
+}

ba0bf13bcf3fcf7725caa06095899edfe306572d
Laksshay <laksshaysehrawat28@gmail.com>
2022-06-15 21:27:51 +0530

    more files

--- new.cpp
+++ new.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
 using namespace std;
-struct person
+
+void TOH(int n, char x, char y, char z)
 {
-    int age;
-    char name[10];
-    int salary;
-};
+    if (n > 0)
+    {
+        TOH(n - 1, x, z, y);
+        cout << "move block from " << x << " to " << y << endl;
+        TOH(n - 1, y, x, z);
+    }
+}
 
 int main()
 {
-    // // doubt 1-
-    // int arr[] = {1, 2, 3, 4};
-    // cout << &arr[0] << endl;
-    // cout << &arr[1] << endl;
-    // cout << "size of int is " << ((&arr[1]) - (&arr[0])) << endl;
-    // // why is the answer not 4?
-    cout << "Hello"
-         << "world";
-    ;
-    ;
-    ;
-    ;
-    return 0;
-}
-
-void move(int n, char src, char des, char aux)
-{
-    if (n > 1)
-    {
-        move(n - 1, src, aux, des);
-    }
+    int n;
+    cout << "enter n - ";
+    cin >> n;
+    TOH(n, 'a', 'b', 'c');
 }

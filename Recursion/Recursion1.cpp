48ce0e5acf5cfb6acd45cb5a8f03181bcfcd97bf
Laksshay <laksshaysehrawat28@gmail.com>
2022-07-23 20:16:18 +0530

    recursion added

--- Recursion/Recursion1.cpp
+++ Recursion/Recursion1.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
-int ways_to_nStair(int n)
+
+int ways_to_nStair(int n, int start = 0)
 {
+    if (start == n)
+    {
+        return 1;
+    }
+    // recursive relation -
+    return ways_to_nStair(n, start + 1);
+}
+void to_destination(int start, int destination)
+{
+    cout << "Currently at " << start << " have to go to " << destination << endl;
+    if (start == destination)
+    {
+        cout << "pahuch gaya";
+        return;
+    }
+    return to_destination(start + 1, destination);
 }
 int main()
 {
-    cout << ways_to_nStair(5) << endl;
+    to_destination(0, 10);
 }

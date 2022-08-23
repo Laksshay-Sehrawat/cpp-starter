fc5e91b0b62bb1d9ee7127a90bfb759eb5cbedbe
Laksshay <laksshaysehrawat28@gmail.com>
2022-08-24 00:02:29 +0530

    `

--- Recursion/Recursion1.cpp
+++ Recursion/Recursion1.cpp
@@ -5,6 +5,7 @@ int ways_to_nStair(int n, int start = 0)
 {
     if (start == n)
     {
+        cout << "destination reached" << endl;
         return 1;
     }
     // recursive relation -
@@ -15,12 +16,29 @@ void to_destination(int start, int destination)
     cout << "Currently at " << start << " have to go to " << destination << endl;
     if (start == destination)
     {
-        cout << "pahuch gaya";
+        cout << "pahuch gaya" << endl;
         return;
     }
     return to_destination(start + 1, destination);
 }
+int fibb(int n, int start = 0)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+        return 1;
+    return fibb(n - 1) + fibb(n - 2);
+}
 int main()
 {
-    to_destination(0, 10);
+    int n;
+    cout << "enter the number till fibb - ";
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cout << fibb(i) << " ";
+    }
+    cout << endl;
 }
--- Recursion/Recursion_in_arrays.cpp
+++ Recursion/Recursion_in_arrays.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-bool sorted_or_not(int *arr, int size)
+bool sorted_or_not(int *arr, int size) // to check if the array is sorted or not using recursion
 {
     if (size <= 1)
     {
@@ -16,7 +16,7 @@ bool sorted_or_not(int *arr, int size)
     }
 }
 
-int sum_array_recursively(int arr[], int size)
+int sum_array_recursively(int arr[], int size) // sum of array recursively
 {
     if (size == 0)
     {
@@ -45,10 +45,37 @@ int linear_search(int arr[], int size, int key)
     }
     index++;
 }
+int binary_serach_recursively(int arr[], int size, int key)
+{
+    cout << "first element of iteration : " << arr[0] << endl;
+    int mid = size / 2;
+    if (size == 1)
+    { // base case
+        return -1;
+    }
+
+    if (arr[mid] == key)
+    {
+        return mid;
+    }
+    if (arr[mid] > key)
+    {
+        return binary_serach_recursively(arr, mid - 1, key);
+    }
+    if (arr[mid] < key)
+    {
+        return binary_serach_recursively(arr + mid + 1, size, key);
+    }
+}
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = sizeof(arr) / sizeof(int);
-    sorted_or_not(arr, size) ? cout << "array sorted\n" : cout << "array not sorted\n";
-    cout << "sum of the array is : " << sum_array_recursively(arr, size) << endl;
+    // sorted_or_not(arr, size) ? cout << "array sorted\n" : cout << "array not sorted\n";
+    // cout << "sum of the array is : " << sum_array_recursively(arr, size) << endl;
+    cout << size << endl;
+    cout << "enter key :  ";
+    int key;
+    cin >> key;
+    cout << "element at : " << binary_serach_recursively(arr, size, key);
 }

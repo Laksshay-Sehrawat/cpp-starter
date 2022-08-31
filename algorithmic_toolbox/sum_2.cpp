47bdc12967f52ae8f2bacb22d815312153e6d970
Laksshay <laksshaysehrawat28@gmail.com>
2022-08-31 21:38:14 +0530

    `

---
+++ algorithmic_toolbox/sum_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // we will find the 2 max numbers in the array
+    int max1 = INT16_MIN, max2 = INT16_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if (arr[i] > max2)
+        {
+            max2 = arr[i];
+        }
+    }
+    long long prod = max2 * max1;
+    cout << prod;
+}
